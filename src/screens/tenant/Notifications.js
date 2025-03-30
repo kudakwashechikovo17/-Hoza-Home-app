4636f4405bd53302b3e351585909f997956eaf03
kudakwashechikovo17 <k.chikovo@alustudent.com>
2025-03-30 04:21:34 +0200

    Create Notifications.js

---
+++ src/screens/tenant/Notifications.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const NotificationsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Notifications Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 18,
+    fontFamily: 'poppins-medium',
+  },
+});
+
+export default NotificationsScreen;
